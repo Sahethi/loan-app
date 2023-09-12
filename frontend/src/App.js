4b57bb9dcd83ae852082300c7216f1aee1e77447
Sahethi <dgsahethi@gmail.com>
2023-09-12 17:49:41 +0530

    api for fetching loan type added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
       <BrowserRouter>
             <Routes>
-              <Route path="/" element= { <Login/>} />
+              <Route path="/login" element= { <Login/>} />
               <Route path="/register" element={ <Register/>}/>
             </Routes>
         </BrowserRouter>
