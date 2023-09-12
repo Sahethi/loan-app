13a11205ac714b56ab68af3b7d3dc70816da1d02
apoorvachawda <apoorvachawda121@gmail.com>
2023-09-12 12:23:30 +0530

    added register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
 function App() {
   return (
     <div>
@@ -7,6 +8,8 @@ function App() {
       <BrowserRouter>
             <Routes>
               <Route path="/" element= { <Login/>} />
+            </Routes>
+            <Routes path="/register" element={ <Register/>}>
             </Routes>
         </BrowserRouter>
       
